668f967c6e70458542a5e0cfe3fa44925f9a2064
NRTnarathip <61742272+NRTnarathip@users.noreply.github.com>
2025-08-24 22:23:14 +0700

    WIP: use external project decima-native

--- ModLoader/DevDebug.cpp
+++ ModLoader/DevDebug.cpp
@@ -1,7 +1,7 @@
 #include "DevDebug.h"
 #include "utils.h"
 #include "types.h"
-#include "RTTI.h"
+#include "extern/decima-native/source/Core/RTTIObject.h"
 
 typedef void (*MyAddWeapon2_t)(void* p1, byte p2);
 MyAddWeapon2_t backupMyAddWeapon2;
@@ -32,7 +32,7 @@ struct PlayerMgr {
 
 typedef PlayerEntity* (*My_GetPlayerEntityByID_t)(PlayerMgr* p1_playerMgr, int p2_id);
 My_GetPlayerEntityByID_t backupMyGetPlayerEntityByID;
-PlayerEntity* My_GetPlayerEntityByID(PlayerMgr* p1_playerMgr, int p2_id) {
+PlayerEntity* My_GetDSPlayerByID(PlayerMgr* p1_playerMgr, int p2_id) {
 	log("Begin My_GetPlayerEntityByID, p1: %p, p2: %d", p1_playerMgr, p2_id);
 	log("player mgr:");
 	log("  total: %d", p1_playerMgr->total);
@@ -44,17 +44,16 @@ PlayerEntity* My_GetPlayerEntityByID(PlayerMgr* p1_playerMgr, int p2_id) {
 		log("  id: %u", playerEnt->id);
 		log("rtti info:");
 		auto rtti = (RTTIObject*)playerEnt;
-		log("rtti vtable rva: 0x%llx", (uint64_t)ConvertAddressToRva(*(void***)rtti));
 		auto type = rtti->GetRTTI();
-		auto name = type->Name();
+		log("type: %p", type);
+		auto name = type->GetName();
+		log("name ptr: %p", name);
 		log("  type name: %s", name.c_str());
-		//auto typeName = type->getTypeName();
-		//void** vtable = *(void***)type;
-		//log("  type name ptr: %p", typeName);
-		//if (typeName) {
-		//	log("  type name cstr: %s", typeName);
-		//	log("  type name MyString: %s", ((MyString*)type->getTypeName())->str);
-		//}
+		auto fields = type->GetAttrs();
+		log("  field count: %d", fields.size());
+		for (auto& field : fields) {
+			log("  field: %s", field.mName);
+		}
 	}
 	return playerEnt;
 }
@@ -63,5 +62,13 @@ void SetupDevDebug()
 {
 	//HookFuncRva(0x278b160, &MyAddWeapon2, &backupMyAddWeapon2);
 	//HookFuncRva(0x2758c80, &DSNodePlayerGetParamFloat, &backupDSNodePlayerGetParamFloat);
-	HookFuncRva(0x23c60b0, &My_GetPlayerEntityByID, &backupMyGetPlayerEntityByID);
+	HookFuncRva(0x23c60b0, &My_GetDSPlayerByID, &backupMyGetPlayerEntityByID);
+
+	// from decima-native/source/main.cpp
+	Offsets::MapSignature("RTTIRefObject::DecrementRef", "40 53 48 83 EC 20 48 8B D9 B8 ? ? ? ? F0 0F C1 41 ? 25 ? ? ? ? 83 F8 01 75 34 8B 41");
+	Offsets::MapSignature("String::FromCString", "40 53 48 83 EC 20 48 8B D9 48 C7 01 00 00 00 00 49 C7 C0 FF FF FF FF");
+	Offsets::MapSignature("String::FromString", "48 89 5C 24 ? 48 89 74 24 ? 57 48 83 EC 20 48 8B 39 48 8B F2 48 8B D9 48 3B 3A 74 54 48 89 6C 24");
+	Offsets::MapSignature("String::~String", "40 53 48 83 EC 20 48 8B 19 48 8D 05 ? ? ? ? 48 83 EB 10 48 3B D8 74 27 B8 ? ? ? ? F0 0F C1 03 0F BA F0 1F 83 F8 01 75 15 48 8B");
+	Offsets::MapSignature("RTTI::GetName", "48 89 5C 24 ? 48 89 74 24 ? 57 48 83 EC 70 48 8B 05 ? ? ? ? 48 33 C4 48 89 44 24 ? 0F B6 41 04 48 8B FA 48 8B F1 83 F8");
+	Offsets::MapSignature("RTTI::ToString", "4C 8B DC 57 41 54 41 55 48 83 EC 70 48 8B 05 ? ? ? ? 48 33 C4 48 89 44 24 ? 4C 8B E9 4D 8B E0 0F B6");
 }
